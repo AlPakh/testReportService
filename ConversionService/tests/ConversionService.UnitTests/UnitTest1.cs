c9154a394c1f9e5a7151d51890c64b15d9fb8db6
AlPakh <alvp2003@yandex.ru>
2026-03-30 01:13:30 +0300

    Added MSTest project

---
+++ ConversionService/tests/ConversionService.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ConversionService.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
